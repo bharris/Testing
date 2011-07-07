16525ff5044c5c28f92b455c573e2edaa39a9262
bharris <bruce@ikayzo.com>
2011-07-07 13:32:11 -1000

    Initializing project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TESTProject::Application.config.secret_token = 'd118d670bd4e6e53cedc0443cf3c66e4d2da58983874dab75f765480e172e19ab12e6d64c4f6f87b2a747cb0ce412cb4b23a3bfa89a0e968cae0adfec390969b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TESTProject::Application.config.session_store :cookie_store, :key => '_TEST Project_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TESTProject::Application.config.session_store :active_record_store
